f1af87d8da7e4375025ac6f84da506e93c6a3fbe
Daniel Jeffery <danielajeffery@gmail.com>
2022-08-15 00:24:11 -0700

    Adding buttons for spells

--- game.js
+++ game.js
@@ -7,15 +7,17 @@
 // popup errors if there are any (help diagnose issues on mobile devices)
 // onerror = (...parameters)=> alert(parameters);
 
-let levelSize, cursor, necromancer;
+let levelSize, cursor, necromancer, skeletonButton, explosionButton;
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameInit() {
-  canvasFixedSize = vec2(720, 1280); // 720p
-  levelSize = vec2(40, 72);
-  cameraPos = levelSize.scale(.5);
-  cursor = new Cursor(vec2(levelSize.x/2, levelSize.y/2))
-  necromancer = new Necromancer(vec2(levelSize.x/2,2));
+    canvasFixedSize = vec2(720, 1280); // 720p
+    levelSize = vec2(40, 72);
+    cameraPos = levelSize.scale(.5);
+    necromancer = new Necromancer(vec2(levelSize.x / 2, 15));
+    cursor = new Cursor(vec2(levelSize.x / 2, levelSize.y / 2))
+    explosionButton = new ExplosionButton(vec2(levelSize.x - 8, 4))
+    skeletonButton = new SkeletonButton(vec2(levelSize.x - 8, 9))
 }
 
 /// ////////////////////////////////////////////////////////////////////////////
@@ -30,8 +32,8 @@ function gameUpdatePost() {
 
 /// ////////////////////////////////////////////////////////////////////////////
 function gameRender() {
-  drawRect(cameraPos, levelSize.scale(2), new Color(.4,.4,.4));
-  drawRect(cameraPos, levelSize, new Color(.1,.1,.1));
+    drawRect(cameraPos, levelSize.scale(2), new Color(.4, .4, .4));
+    drawRect(cameraPos, levelSize, new Color(.1, .1, .1));
 }
 
 /// ////////////////////////////////////////////////////////////////////////////
--- gameObjects.js
+++ gameObjects.js
@@ -1,43 +1,70 @@
+// Player
 class Necromancer extends EngineObject {
     constructor(pos) {
         super(pos, vec2(4), 0);
 
-        // set to collide
         this.setCollision(0, 0);
-        this.mass = 0;
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////
+// Cursor
 class Cursor extends EngineObject {
-  constructor(pos) {
-    super(pos, vec2(2), 1, tileSizeDefault, 0);
+    constructor(pos) {
+        super(pos, vec2(4), 1, tileSizeDefault, 0);
 
-    // set to collide
-    this.setCollision(0, 0);
-    this.mass = 0;
-    this.addChild(new ParticleEmitter(
-        vec2(0, 0), 0, objectDefaultSize, 0, 2, 0,  // pos, angle, emitSize, emitTime, emitRate, emiteCone
-        0, tileSizeDefault,                              // tileIndex, tileSize
-        new Color(1,0,0),   new Color(0,0,0),   // colorStartA, colorStartB
-        new Color(1,0,0,0), new Color(0,0,0,0), // colorEndA, colorEndB
-        2, .2, .2, .1, .05,  // particleTime, sizeStart, sizeEnd, particleSpeed, particleAngleSpeed
-        .99, 1, 1, PI, .05,  // damping, angleDamping, gravityScale, particleCone, fadeRate,
-        .5, 0                // randomness, collide, additive, randomColorLinear, renderOrder
-    ), vec2(0,-0.5), PI);
-  }
+        // set to collide
+        this.setCollision(0, 0);
+        this.addChild(new ParticleEmitter(
+            vec2(0, 0), 0, objectDefaultSize, 0, 4, 0,  // pos, angle, emitSize, emitTime, emitRate, emiteCone
+            0, tileSizeDefault,                              // tileIndex, tileSize
+            new Color(1, 0, 0), new Color(0, 0, 0),   // colorStartA, colorStartB
+            new Color(1, 0, 0, 0), new Color(0, 0, 0, 0), // colorEndA, colorEndB
+            2, 0.5, .2, .1, .05,  // particleTime, sizeStart, sizeEnd, particleSpeed, particleAngleSpeed
+            .99, 1, 1, PI, .05,  // damping, angleDamping, gravityScale, particleCone, fadeRate,
+            .5, 0                // randomness, collide, additive, randomColorLinear, renderOrder
+        ), vec2(0, -0.5), PI);
 
-  update() {
-    if (isUsingGamepad) {
-      // control with gamepad
-      this.pos.x += gamepadStick(0).x;
-      this.pos.y += gamepadStick(0).y;
-    } else {
-      // move to mouse
-      this.pos.x = mousePos.x;
-      this.pos.y = mousePos.y;
+        this.renderOrder = 20
     }
-    this.pos.x = clamp(this.pos.x, this.size.x / 2, levelSize.x - this.size.x / 2);
-    this.pos.y = clamp(this.pos.y, this.size.y / 2, levelSize.y - this.size.y / 2);
-  }
+
+    update() {
+        if (isUsingGamepad) {
+            // control with gamepad
+            this.pos.x += gamepadStick(0).x;
+            this.pos.y += gamepadStick(0).y;
+        } else {
+            // move to mouse
+            this.pos.x = mousePos.x;
+            this.pos.y = mousePos.y;
+        }
+        this.pos.x = clamp(this.pos.x, this.size.x / 2, levelSize.x - this.size.x / 2);
+        this.pos.y = clamp(this.pos.y, this.size.y / 2, levelSize.y - this.size.y / 2);
+    }
+}
+
+// Buttons
+class Button extends EngineObject {
+    constructor(pos, text, color) {
+        super(pos, vec2(0, 0), 0);
+        this.text = text;
+        this.color = color;
+        this.font = new FontImage();
+    }
+
+    render() {
+        drawRect(vec2(this.pos.x, this.pos.y - 1.5), vec2( 15, 4), this.color)
+        this.font.drawText(this.text, this.pos, 0.2, true);
+    }
+}
+
+class SkeletonButton extends Button {
+    constructor(pos) {
+        super(pos, "Summon\nSkeleton", new Color(1, 0, 0));
+    }
+}
+
+class ExplosionButton extends Button {
+    constructor(pos) {
+        super(pos, "Corpse\nExplosion", new Color(1, 0, 0));
+    }
 }
